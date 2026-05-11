8ac3b1b71a760bea159523857ed56909bcfacab0
LechintanJoshua <jlechintan79@gmail.com>
2026-05-11 06:08:20 +0300

    Implement Open and createFileSync
    cFS creates a file if it doesn't exist
    and syncs the directory it is in
    Open calls the method and sets struct fd

--- internal/kv.go
+++ internal/kv.go
@@ -1,6 +1,11 @@
 package internal
 
-import "syscall"
+import (
+	"fmt"
+	"os"
+	"path"
+	"syscall"
+)
 
 type KV struct {
 	Path string // numele fisierului
@@ -8,6 +13,20 @@ type KV struct {
 	fd   int
 	tree BTree
 	// mai mult
+}
+
+// Open deschide fisierul din adresa Path
+// si verifica daca aceasta a avut succes
+func (db *KV) Open() error {
+	fd, err := createFileSync(db.Path)
+
+	if err != nil {
+		return err
+	}
+
+	db.fd = fd
+
+	return nil
 }
 
 // Get apeleaza metoda interna a tree si returneaza
@@ -33,7 +52,7 @@ func (db *KV) Del(key []byte) (bool, error) {
 // updateFile actualizeaza fisierul si verifica erorile
 // scrie nodurile in pagini, sincronizeaza, actualizeaza
 // radacina si sincronizeaza din nou sa fie persistent
-// (double-write)
+// (two-phase-update)
 func updateFile(db *KV) error {
 	// 1. Scrie noduri
 	if err := writePages(db); err != nil {
@@ -53,4 +72,36 @@ func updateFile(db *KV) error {
 	// 4. fsync sa fie totul persistent
 
 	return syscall.Fsync(db.fd)
+}
+
+// createFileSync creaza un fisier nou,
+// seteaza masca la -rw-r--r-- si sincronizeaza
+// directorul in care se afla
+func createFileSync(file string) (int, error) {
+	// obtine file descriptorul directorului
+	flags := os.O_RDONLY | syscall.O_DIRECTORY
+	dirfd, err := syscall.Open(path.Dir(file), flags, 0o644)
+
+	if err != nil {
+		return -1, fmt.Errorf("open director: %w", err)
+	}
+
+	defer syscall.Close(dirfd)
+
+	// deschide sau creaza fisierul
+	flags = os.O_RDWR | os.O_CREATE
+	fd, err := syscall.Openat(dirfd, path.Base(file), flags, 0o644)
+
+	if err != nil {
+		return -1, fmt.Errorf("open file: %w", err)
+	}
+
+	// fsync directorul
+
+	if err = syscall.Fsync(dirfd); err != nil {
+		_ = syscall.Close(fd) // s-ar putea sa lase un fisier gol
+		return -1, fmt.Errorf("fsync directory: %w", err)
+	}
+
+	return fd, nil
 }
