bf0efc2f5784b0af0e42e4de91cf4d4f9cf6781f
LechintanJoshua <jlechintan79@gmail.com>
2026-06-02 14:50:24 +0300

    Add Put function to add a page to cache and table Update LRUList and check it's size

--- internal/lru.go
+++ internal/lru.go
@@ -1,8 +1,10 @@
 package internal
 
 const (
-	TABLE_SIZE = 128
-	CACHE_CAP  = 100
+	TABLE_SIZE  = 128
+	CACHE_CAP   = 100
+	INSERT_HASH = 1
+	UPDATE_HASH = 2
 )
 
 type Bucket struct {
@@ -89,6 +91,22 @@ func InitLRUCache() *LRUCache {
 	return cache
 }
 
+// moveNodeHead muta nodul primit ca parametru
+// daca acesta a fost un nod deja existent, il scoate de
+// la locul lui si reface legaturile
+func moveNodeHead(cache *LRUCache, hitNode *LRUNode, mode int) {
+	head := cache.list.head
+	if mode == UPDATE_HASH {
+		hitNode.prev.next = hitNode.next
+		hitNode.next.prev = hitNode.prev
+	}
+
+	hitNode.next = head.next
+	head.next.prev = hitNode
+	hitNode.prev = head
+	head.next = hitNode
+}
+
 // Get verifica daca o cheie este stocata in hashtable
 // si returneaza valoarea ei, respectiv un slice gol
 // totodata in cazul de Cache Hit actualizeaza
@@ -108,13 +126,72 @@ func (cache *LRUCache) Get(key uint64) []byte {
 	}
 
 	hitNode := curr.lruPointer
-	head := cache.list.head
-	hitNode.prev.next = hitNode.next
-	hitNode.next.prev = hitNode.prev
-	hitNode.next = head.next
-	head.next.prev = hitNode
-	hitNode.prev = head
-	head.next = hitNode
-
+	moveNodeHead(cache, hitNode, UPDATE_HASH)
 	return hitNode.pageData
+}
+
+// insertUpdateTable insereaza sau actualizeaza o pagina in hashtable
+// Returneaza tipul (insert/update) si nodul inserat/actualizat
+func insertUpdateTable(cache *LRUCache, key uint64, data []byte) (int, *LRUNode) {
+	hash := hashKey(key)
+	curr := cache.table[hash].head.next
+
+	for curr != nil {
+		if curr.pagePtr == key {
+			hitNode := curr.lruPointer
+			hitNode.pageData = data
+			return UPDATE_HASH, hitNode
+		}
+		curr = curr.next
+	}
+
+	lruNode := &LRUNode{
+		pagePtr:  key,
+		pageData: data,
+	}
+	buckNode := &BucketNode{
+		pagePtr:    key,
+		lruPointer: lruNode,
+	}
+
+	cache.size++
+	cache.table[hash].tail.next = buckNode
+	cache.table[hash].tail = buckNode
+
+	return INSERT_HASH, lruNode
+}
+
+// removeNodeListTable sterge un nod din lista de cache si din hashtable
+func removeNodeListTable(cache *LRUCache) {
+	key := cache.list.tail.prev.pagePtr
+	cache.size--
+	newLast := cache.list.tail.prev.prev
+	newLast.next = cache.list.tail
+	cache.list.tail.prev = newLast
+
+	hash := hashKey(key)
+	curr := cache.table[hash].head
+	tail := cache.table[hash].tail
+	for curr.next.pagePtr != key {
+		curr = curr.next
+	}
+
+	deleted := curr.next
+	if deleted == tail {
+		tail = curr
+	}
+
+	curr.next = deleted.next
+}
+
+// Put adauga o pagina in hastable si in lista de caching
+// in cazul existentei, actualizeaza valoarea stocata
+// Daca capacitatea listei LRU depaseste 100, elimina
+// ultima pagina, aceia find si cea mai putin utilizata
+func (cache *LRUCache) Put(key uint64, data []byte) {
+	mode, node := insertUpdateTable(cache, key, data)
+	moveNodeHead(cache, node, mode)
+	if cache.size > cache.capacity {
+		removeNodeListTable(cache)
+	}
 }
