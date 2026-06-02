69dd2d1cfb45c0aaaef44e155e1488c78e2f5e43
LechintanJoshua <jlechintan79@gmail.com>
2026-06-02 12:12:10 +0300

    Create LRUCache struct and add Init function
    Implement FNV-1a algorithm for hashing a key

--- internal/hashtable.go
+++ internal/hashtable.go
@@ -1,6 +1,9 @@
 package internal
 
-const TABLE_SIZE = 128
+const (
+	TABLE_SIZE = 128
+	CACHE_CAP  = 100
+)
 
 type HashTable struct {
 	table [TABLE_SIZE]*Bucket
@@ -29,6 +32,13 @@ type LRUList struct {
 	tail *LRUNode
 }
 
+type LRUCache struct {
+	capacity int
+	size     int
+	table    *HashTable
+	list     *LRUList
+}
+
 // initBucketSentinel este folosita pentru a crea un nod santinela
 // in lista inlantuita din bucket
 func initBucketSentinel() *Bucket {
@@ -41,11 +51,54 @@ func initBucketSentinel() *Bucket {
 
 // InitializeTable initializeaza fiecare valoare din table cu
 // o lista inlantuita ce contine un singur nod gol santinela
-func InitializeTable() *HashTable {
+func initializeTable() *HashTable {
 	ht := &HashTable{}
 	for i := range ht.table {
 		ht.table[i] = initBucketSentinel()
 	}
 
 	return ht
+}
+
+// Functie pentru transformarea unei chei intr-un
+// hash folosind algoritmul FNV-1a pentur 32bits
+func hashKey(key []byte) uint32 {
+	var offset uint32 = 2166136261
+	var prime uint32 = 16777619
+
+	for _, b := range key {
+		offset ^= uint32(b)
+		offset *= prime
+	}
+
+	return offset & (TABLE_SIZE - 1)
+}
+
+// initLRUList initializeaza lista dubla inlantuita pentru
+// mentinearea nodurilor folosite in cache
+// lista este initializata cu doua noduri santinela goale
+// tail si head pentru a evita edge-case-urile
+func initLRUList() *LRUList {
+	headNode := &LRUNode{}
+	tailNode := &LRUNode{}
+	headNode.next = tailNode
+	tailNode.prev = headNode
+
+	return &LRUList{
+		head: headNode,
+		tail: tailNode,
+	}
+}
+
+// InitLRUCache initializeaza structura folosita pentrru
+// un caching de tip Least Recently Used
+// capacitatea este hardcodata la 100
+// nodurile santinela vor fi ignorate
+func InitLRUCache() *LRUCache {
+	return &LRUCache{
+		capacity: CACHE_CAP,
+		size:     0,
+		table:    initializeTable(),
+		list:     initLRUList(),
+	}
 }
